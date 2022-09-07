a5acb25c132a5e9d0ac17e088cee6f20109b5ea6
JoeyMcQuade <joeymmcquade@outlook.com>
2022-09-07 20:15:08 +0100

    :rocket: Added type hints :rocket:

--- get_stock_data.py
+++ get_stock_data.py
@@ -1,5 +1,7 @@
 import requests
-from secrets import SECRET_API_KEY
+from decouple import config
+from typing import List
+
 """
     Gets some stock data from an api and converts it into a graph.
     <<GOAL>> Send a request with param of <<str: symbol>> (company name_ then returns the relevant data with that 
@@ -8,13 +10,13 @@ from secrets import SECRET_API_KEY
 """
 
 
-"""
-Gets a list of suggestions for ticker symbols based on user input.
-"""
-def get_ticker_symbol(company):
+def get_ticker_symbol(company: str) -> List[dict]:
+    """
+    Gets a list of suggestions for ticker symbols based on user input.
+    """
     # send a GET request with the company name as keyword in the url
     res = requests.get(f'https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords={company}'
-                       f'&apikey={SECRET_API_KEY}')
+                       f'&apikey={config("SECRET_API_KEY")}')
     ticker_symbol = res.json()["bestMatches"]
     return [{
         'symbol': current_selected_data['1. symbol'],
@@ -22,15 +24,15 @@ def get_ticker_symbol(company):
     } for current_selected_data in ticker_symbol[:5]]
 
 
-"""
-Gets the stock data from the rapidapi link.
-"""
-def get_data_from_api(ticker_symbol):
+def get_data_from_api(ticker_symbol: int) -> dict:
+    """
+    Gets the stock data from the rapidapi link.
+    """
     url = "https://alpha-vantage.p.rapidapi.com/query"
     querystring = {"function": "TIME_SERIES_DAILY_ADJUSTED", "symbol": f"{ticker_symbol}", "outputsize": "compact",
                    "datatype": "json"}
     headers = {
-        "X-RapidAPI-Key": f"{SECRET_API_KEY}",
+        "X-RapidAPI-Key": f"{config('SECRET_API_KEY')}",
         "X-RapidAPI-Host": "alpha-vantage.p.rapidapi.com"
     }
 
@@ -38,7 +40,10 @@ def get_data_from_api(ticker_symbol):
     return response.json()['Time Series (Daily)']
 
 
-def get_most_recent_days(raw_data, day_range):
+def get_most_recent_days(raw_data: dict, day_range: int) -> List[dict]:
+    """
+    Returns relevant stock data for the date range provided.
+    """
     return_values = [{
         'date': current_selected_data,
         'open': raw_data[current_selected_data]['1. open'],
